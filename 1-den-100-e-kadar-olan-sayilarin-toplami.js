5c3edcd63946720c3cdec9d1b1904114171cfa41
Ender король <94347285+marijua@users.noreply.github.com>
2022-02-01 13:55:10 +0300

    Add files via upload

---
+++ 1-den-100-e-kadar-olan-sayilarin-toplami.js
@@ -0,0 +1,9 @@
+// Küçüktür işaret : <
+// Büyüktür İşareti : >
+let i;
+let toplam = 0;
+for(i = 0;  i < 100; i++)
+{
+  toplam = toplam+i;
+  console.log(`1'den 100'e kadar olan sayıların toplamı = ${toplam}`);
+}
