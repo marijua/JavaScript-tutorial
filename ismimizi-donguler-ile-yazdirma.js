3d6d5d269adb100e3f2f75c5907af04de882ed18
Ender король <94347285+mizahx@users.noreply.github.com>
2022-02-01 13:43:38 +0300

    Add files via upload

---
+++ ismimizi-donguler-ile-yazdirma.js
@@ -0,0 +1,17 @@
+// Küçüktür işaret : <
+// Büyüktür İşareti : >
+
+ let isim = prompt("İsminiz");
+
+for(let i = 0; i < 5; i++)
+{
+  console.log(`${isim}`);
+}
+
+let i = 0;
+
+while(i < 5)
+{
+  i++;
+  console.log(`${isim}`);
+}
